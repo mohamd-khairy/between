5850784f45cfbfa7e2397aaffc4bd4cee3b60383
mohamed-khairy <m.khairy@evntoo.com>
2021-06-27 14:13:29 +0200

    make admin lte with two language

---
+++ app/Helpers/helper.php
@@ -0,0 +1,7 @@
+<?php
+
+
+function current_url()
+{
+    return url()->current();
+}
---
+++ resources/lang/vendor/adminlte/ar/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'MAIN NAVIGATION',
+    'blog'                          => 'المقالات',
+    'pages'                         => 'الصفحات',
+    'account_settings'              => 'الاعدادات',
+    'profile'                       => 'الصفحه الشخصيه',
+    'change_password'               => 'تغيير كلمه السر',
+    'multilevel'                    => 'تعدد المستويات',
+    'level_one'                     => 'المستوي 1',
+    'level_two'                     => 'المستوي 2',
+    'level_three'                   => 'المستوي 3',
+    'labels'                        => 'العناوين',
+    'important'                     => 'مهم',
+    'warning'                       => 'تحذير',
+    'information'                   => 'معلومات',
+];
