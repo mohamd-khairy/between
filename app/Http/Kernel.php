18329a0c3a422a04aac66e866991a9ae02b2586b
mohamd-khairy <mohamed.khairy.eg@gmail.com>
2021-07-03 15:54:12 +0200

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\SetLang::class
     ];
 
     /**
@@ -43,7 +44,6 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\SetLang::class
         ],
     ];
 
