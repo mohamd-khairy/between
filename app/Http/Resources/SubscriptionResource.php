05b6569437b41ae45084757412d164c78d0d1c11
mohamd-khairy <mohamed.khairy.eg@gmail.com>
2021-12-01 13:32:26 +0200

    fix

--- app/Http/Resources/SubscriptionResource.php
+++ app/Http/Resources/SubscriptionResource.php
@@ -15,10 +15,10 @@ class SubscriptionResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'ingredient_ids' => $this->ingredient_ids ?? null,
-            'protien_ids' => $this->protien_ids ?? null,
-            'snack_ids' => $this->snack_ids ?? null,
-            'carb_ids' => $this->carb_ids ?? null,
+            'ingredient_ids' => $this->ingredient_ids ? json_decode($this->ingredient_ids) : null,
+            'protien_ids' => $this->protien_ids ? json_decode($this->protien_ids) : null,
+            'snack_ids' => $this->snack_ids ? json_decode($this->snack_ids) : null,
+            'carb_ids' => $this->carb_ids ? json_decode($this->carb_ids) : null,
             'user_id' => $this->user_id ?? null,
             'target_id' => $this->target_id ?? null,
             'meal_number_id' => $this->meal_number_id ?? null,
@@ -26,15 +26,15 @@ class SubscriptionResource extends JsonResource
             'dish_id' => $this->dish_id ?? null,
             'food_id' => $this->food_id ?? null,
             'address_id' => $this->address_id ?? null,
-            'start_date' => $this->start_date ?? null,
-            'end_date' => $this->end_date ?? null,
-            'user' => $this->user ?? null,
-            'target' => $this->target ?? null,
+            'start_date' => $this->start_date ? date('Y-m-d', strtotime($this->start_date)) : null,
+            'end_date' => $this->end_date ? date('Y-m-d', strtotime($this->end_date)) :  null,
+            'user' => $this->user ? new UserResource($this->user) : null,
+            'target' => $this->target ? new TargetResource($this->target) : null,
             'meal_number' => $this->meal_number ?? null,
             'prefered_time' => $this->prefered_time ?? null,
-            'dish' => $this->dish ?? null,
-            'food' => $this->food ?? null,
-            'address' => $this->address ?? null,
+            'dish' => $this->dish ? new DishResource($this->dish) : null,
+            'food' => $this->food ? new FoodResource($this->food) : null,
+            'address' => $this->address ? new AddressResource($this->address) : null,
         ];
     }
 }
